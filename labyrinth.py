54d1af2310a1dea39968f720a3d75c4c05763475
Zvonimir Erdelja <zvonimir.erdelja@gmail.com>
2019-12-18 20:42:41 +0100

    labyrinth module

---
+++ labyrinth.py
@@ -0,0 +1,122 @@
+from enum import Enum
+from typing import Iterable, Dict, Tuple
+
+
+class Coordinate:
+    def __init__(self, x : int, y : int):
+        self.x = x
+        self.y = y
+
+    def __repr__(self) -> str:
+        return f'<Coordinate {self.x}, {self.y}>'
+
+    def _data(self) -> Tuple[int, int]:
+        return self.x, self.y
+
+    def __iter__(self) -> Iterable[int]:
+        return iter(self._data())
+
+    def __eq__(self, other) -> bool:
+        return self._data() == other._data()
+
+    def __hash__(self) -> int:
+        return hash(self._data())
+
+    def up(self) -> 'Coordinate':
+        return Coordinate(self.x, self.y - 1)
+
+    def down(self) -> 'Coordinate':
+        return Coordinate(self.x, self.y + 1)
+
+    def left(self) -> 'Coordinate':
+        return Coordinate(self.x - 1, self.y)
+
+    def right(self) -> 'Coordinate':
+        return Coordinate(self.x + 1, self.y)
+
+
+class Direction(Enum):
+    NORTH = 'N'
+    SOUTH = 'S'
+    WEST = 'W'
+    EAST = 'E'
+
+    def move_coord(self, coord : Coordinate) -> Coordinate:
+        return {
+            Direction.NORTH: coord.up,
+            Direction.SOUTH: coord.down,
+            Direction.WEST: coord.left,
+            Direction.EAST: coord.right,
+        }[self]()
+
+    def opposite(self) -> 'Direction':
+        return {
+            Direction.NORTH: Direction.SOUTH,
+            Direction.SOUTH: Direction.NORTH,
+            Direction.EAST:  Direction.WEST,
+            Direction.WEST:  Direction.EAST
+        }[self]
+
+
+class MapObject(Enum):
+    EMPTY = '.'
+    WALL = '#'
+    TARGET = '$'
+
+
+class AreaMap:
+    def __init__(self, data : Dict[Coordinate, MapObject] = None):
+        self.data = {} if data is None else data.copy()
+
+    def peek(self, coord : Coordinate, *, default=None) -> MapObject:
+        if coord in self.data:
+            return self.data[coord]
+        return default
+
+    def __getitem__(self, coord : Coordinate) -> MapObject:
+        return self.peek(coord, default=MapObject.WALL)
+
+    def __setitem__(self, coord : Coordinate, value : MapObject):
+        self.data[coord] = value
+
+    def __iter__(self) -> Iterable[Coordinate]:
+        return iter(self.data)
+
+    def get_target_locations(self) -> Iterable[Coordinate]:
+        return [coord for coord in self if self[coord] == MapObject.TARGET]
+
+    def __str__(self):
+        xs, ys = map(set, zip(*self))
+        minx = min(xs)
+        maxx = max(xs)
+        miny = min(ys)
+        maxy = max(ys)
+        return '\n'.join(
+            ''.join(self[Coordinate(x, y)].value
+                    if Coordinate(x, y) in self
+                    else ' '
+                    for x in range(minx, maxx + 1))
+            for y in range(miny, maxy + 1))
+
+
+def find_distances(area : AreaMap, start_coord : Coordinate) -> Dict[Coordinate, int]:
+    distance = 0
+    rtn = {start_coord : distance}
+    leaf_coordinates = [start_coord]
+
+    while leaf_coordinates:
+        next_leaves = []
+        for coord in leaf_coordinates:
+            if area[coord] == MapObject.TARGET:
+                continue
+            for next_coord in [d.move_coord(coord) for d in Direction]:
+                if next_coord in rtn:
+                    continue
+                if area[next_coord] == MapObject.WALL:
+                    continue
+                next_leaves.append(next_coord)
+        distance += 1
+        for leaf in next_leaves:
+            rtn[leaf] = distance
+        leaf_coordinates = next_leaves
+    return rtn
--- solve15.py
+++ solve15.py
@@ -2,29 +2,8 @@ from enum import Enum
 
 from intcode import IntcodeRunner
 from input_reader import read_comma_separated_integers
+from labyrinth import MapObject, AreaMap, Coordinate, Direction, find_distances
 
-
-class Direction(Enum):
-    NORTH = 1
-    SOUTH = 2
-    WEST = 3
-    EAST = 4
-
-    def move_coord(self, x, y):
-        return {
-            Direction.NORTH: (x, y - 1),
-            Direction.SOUTH: (x, y + 1),
-            Direction.EAST:  (x + 1, y),
-            Direction.WEST:  (x - 1, y)
-        }[self]
-
-    def back(self):
-        return {
-            Direction.NORTH: Direction.SOUTH,
-            Direction.SOUTH: Direction.NORTH,
-            Direction.EAST:  Direction.WEST,
-            Direction.WEST:  Direction.EAST
-        }[self]
 
 
 class Status(Enum):
@@ -33,108 +12,73 @@ class Status(Enum):
     FOUND = 2
 
 
-class MapObject(Enum):
-    UNKNOWN = 1
-    EMPTY = 2
-    WALL = 3
-    OXYGEN_SYSTEM = 4
-
-
-class AreaMap(dict):
-    def __init__(self):
-        super().__init__()
-        self.distances = {}
-
-    def __getitem__(self, coord):
-        return self.get(coord, MapObject.UNKNOWN)
-
-    def get_available_directions(self, x, y):
-        for direction in Direction:
-            if self[direction.move_coord(x, y)] != MapObject.WALL:
-                yield direction
-
-    def get_locations(self, map_object):
-        return [coord for coord, obj in self.items() if obj == map_object]
-
-
 class DroidRemote:
     def __init__(self):
         self.ic = IntcodeRunner(read_comma_separated_integers('day15input.txt'))
 
     def move(self, direction):
-        self.ic.communicate(direction.value)
+        command = {
+            Direction.NORTH: 1,
+            Direction.SOUTH: 2,
+            Direction.WEST: 3,
+            Direction.EAST: 4
+        }[direction]
+        self.ic.communicate(command)
         return Status(self.ic.communicate())
 
 
-class FakeDroid:
-    def __init__(self, area_map, initial_coord):
-        self.area_map = area_map
-        self.coord = initial_coord
-
-    def move(self, direction):
-        next_coord = direction.move_coord(*self.coord)
-        assert self.area_map[next_coord] != MapObject.UNKNOWN
-        if self.area_map[next_coord] == MapObject.WALL:
-            return Status.WALL
-        else:
-            self.coord = next_coord
-            if self.area_map[next_coord] == MapObject.OXYGEN_SYSTEM:
-                return Status.FOUND
-            else:
-                return Status.MOVED
+def get_available_directions(area, coord):
+    for direction in Direction:
+        if area.peek(direction.move_coord(coord)) != MapObject.WALL:
+            yield direction
 
 
-def explore_depth_first(coord, area, droid, distance):
-    directions = list(area.get_available_directions(*coord))
+def explore_depth_first(coord, area, droid, distance, distances=None):
+    if distances is None:
+        distances = {coord: distance}
+    directions = list(get_available_directions(area, coord))
     for direction in directions:
-        next_coord = direction.move_coord(*coord)
-        if area[next_coord] == MapObject.WALL:
+        next_coord = direction.move_coord(coord)
+        if area.peek(next_coord) == MapObject.WALL:
             continue
-        if area[next_coord] != MapObject.UNKNOWN:
-            if area.distances[next_coord] <= distance + 1:
-                continue
+        if distances.get(next_coord, float('inf')) <= distance + 1:
+            continue
         status = droid.move(direction)
-        area.distances[next_coord] = distance + 1
+        distances[next_coord] = distance + 1
         if status == Status.WALL:
             area[next_coord] = MapObject.WALL
         else:
             if status == Status.FOUND:
-                area[next_coord] = MapObject.OXYGEN_SYSTEM
+                area[next_coord] = MapObject.TARGET
             else:
                 area[next_coord] = MapObject.EMPTY
-            explore_depth_first(next_coord, area, droid, distance + 1)
-            droid.move(direction.back())
+            explore_depth_first(next_coord, area, droid, distance + 1, distances)
+            droid.move(direction.opposite())
+    return distances
 
 
 def puzzle1():
     droid = DroidRemote()
     area = AreaMap()
-    coord = (0, 0)
+    coord = Coordinate(0, 0)
     area[coord] = MapObject.EMPTY
-    area.distances[coord] = 0
-    explore_depth_first(coord, area, droid, 0)
-    oxygen_coord, = area.get_locations(MapObject.OXYGEN_SYSTEM)
-    return area.distances[oxygen_coord]
+    distances = explore_depth_first(coord, area, droid, 0)
+    oxygen_coord, = area.get_target_locations()
+    return distances[oxygen_coord]
 
 
 def puzzle2():
     droid = DroidRemote()
     area = AreaMap()
-    coord = (0, 0)
+    coord = Coordinate(0, 0)
     area[coord] = MapObject.EMPTY
-    area.distances[coord] = 0
     explore_depth_first(coord, area, droid, 0)
-    oxygen_coord, = area.get_locations(MapObject.OXYGEN_SYSTEM)
+    oxygen_coord, = area.get_target_locations()
 
-    fake_droid = FakeDroid(area, oxygen_coord)
-    area = AreaMap()
-    area[oxygen_coord] = MapObject.OXYGEN_SYSTEM
-    area.distances[oxygen_coord] = 0
-    explore_depth_first(oxygen_coord, area, fake_droid, 0)
+    area[oxygen_coord] = MapObject.EMPTY
+    distances = find_distances(area, oxygen_coord)
 
-    return max(
-        distance for coord, distance in area.distances.items()
-        if area[coord] == MapObject.EMPTY)
+    return max(distances.values())
 
 
 if __name__ == "__main__":
